0e3a1c49d2b96cf64a6bbe0ad754a72273b9c1c1
ShnaiderPavel <shnaiderpasha@gmail.com>
2015-07-16 17:01:36 +0300

    последнее

--- small-screwdriver.py
+++ small-screwdriver.py
@@ -1,10 +1,19 @@
 # encoding: utf8
+
 import sys
-from PySide.QtGui import QApplication
+from PySide.QtGui import QApplication, QPushButton
+
+
+class Point(object):
+    def __init__(self):
+        pass
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-    print 'test'
+    button = QPushButton(u"Выход")
+    button.show()
+    button.clicked.connect(app.quit)
 
     sys.exit(app.exec_())
