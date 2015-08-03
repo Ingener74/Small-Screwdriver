0ff3747209fcf71d439e925579351e45b6ee6553
ShnaiderPavel <shnaiderpasha@gmail.com>
2015-08-03 15:28:37 +0300

    переделал тесты

--- SmallScrewdriver/test_bin.py
+++ SmallScrewdriver/test_bin.py
@@ -2,63 +2,61 @@ from unittest import TestCase
 import math
 from SmallScrewdriver import Bin, Size, Point, Rect
 
-__author__ = 'Pavel'
-
 
 class TestBin(TestCase):
     def test_append1(self):
         b = Bin(Size(256, 256), Point(0, 0))
 
-        assert b.append(Rect(Point(0, 0), Size(10, 10)))
-        assert len(b.images) == 1
+        self.assertTrue(b.append(Rect(Point(0, 0), Size(10, 10))))
+        self.assertEqual(len(b.images), 1)
         # area = 100
 
-        assert b.append(Rect(Point(10, 10), Size(100, 100)))
-        assert len(b.images) == 2
+        self.assertTrue(b.append(Rect(Point(10, 10), Size(100, 100))))
+        self.assertEqual(len(b.images), 2)
         # area == 10100
 
-        assert b.append(Rect(Point(10, 10), Size(100, 100)))
-        assert len(b.images) == 3
+        self.assertTrue(b.append(Rect(Point(10, 10), Size(100, 100))))
+        self.assertEqual(len(b.images), 3)
         # area == 20100
 
-        assert b.append(Rect(Point(10, 10), Size(100, 100)))
-        assert len(b.images) == 4
+        self.assertTrue(b.append(Rect(Point(10, 10), Size(100, 100))))
+        self.assertEqual(len(b.images), 4)
         # area == 30100
 
-        assert b.append(Rect(Point(10, 10), Size(100, 100)))
-        assert len(b.images) == 5
+        self.assertTrue(b.append(Rect(Point(10, 10), Size(100, 100))))
+        self.assertEqual(len(b.images), 5)
         # area == 40100
 
-        assert b.append(Rect(Point(10, 10), Size(50, 50)))
-        assert len(b.images) == 6
+        self.assertTrue(b.append(Rect(Point(10, 10), Size(50, 50))))
+        self.assertEqual(len(b.images), 6)
         # area == 42600
 
-        assert b.append(Rect(Point(10, 10), Size(50, 50)))
-        assert len(b.images) == 7
+        self.assertTrue(b.append(Rect(Point(10, 10), Size(50, 50))))
+        self.assertEqual(len(b.images), 7)
         # area == 45100
 
-        assert b.append(Rect(Point(10, 10), Size(50, 50)))
-        assert len(b.images) == 8
+        self.assertTrue(b.append(Rect(Point(10, 10), Size(50, 50))))
+        self.assertEqual(len(b.images), 8)
         # area == 47600
 
-        assert b.append(Rect(Point(10, 10), Size(50, 50)))
-        assert len(b.images) == 9
+        self.assertTrue(b.append(Rect(Point(10, 10), Size(50, 50))))
+        self.assertEqual(len(b.images), 9)
         # area == 50100
 
         # assert b.append(Rect(Point(10, 10), Size(100, 100)))
         # assert len(b.images) == 10
 
-        assert not b.append(Rect(Point(0, 0), Size(512, 512)))
-        assert len(b.images) == 9
+        self.assertFalse(b.append(Rect(Point(0, 0), Size(512, 512))))
+        self.assertEqual(len(b.images), 9)
 
     def test_fillLevel(self):
         b = Bin(Size(256, 256), Point(0, 0))
 
-        b.append(Rect(Point(0, 0), Size(10, 10)))
-        assert math.fabs(b.fillLevel() - (10. * 10.) / (256. * 256.)) < 1e-6
+        self.assertTrue(b.append(Rect(Point(0, 0), Size(10, 10))))
+        self.assertLess(math.fabs(b.fillLevel() - (10. * 10.) / (256. * 256.)), 1e-6)
 
-        b.append(Rect(Point(0, 0), Size(20, 20)))
-        assert math.fabs(b.fillLevel() - (10. * 10. + 20. * 20.) / (256. * 256.)) < 1e-6
+        self.assertTrue(b.append(Rect(Point(0, 0), Size(20, 20))))
+        self.assertLess(math.fabs(b.fillLevel() - (10. * 10. + 20. * 20.) / (256. * 256.)), 1e-6)
 
-        b.append(Rect(Point(0, 0), Size(40, 40)))
-        assert math.fabs(b.fillLevel() - (10. * 10. + 20. * 20. + 40. * 40.) / (256. * 256.)) < 1e-6
+        self.assertTrue(b.append(Rect(Point(0, 0), Size(40, 40))))
+        self.assertLess(math.fabs(b.fillLevel() - (10. * 10. + 20. * 20. + 40. * 40.) / (256. * 256.)), 1e-6)
--- SmallScrewdriver/test_size.py
+++ SmallScrewdriver/test_size.py
@@ -1,19 +1,17 @@
 from unittest import TestCase
 from SmallScrewdriver import Size
 
-__author__ = 'Pavel'
-
 
 class TestSize(TestCase):
     def test_area(self):
         size = Size(100, 100)
-        assert size.area() == 10000
+        self.assertEqual(size.area(), 10000)
 
         size = Size(10, 10)
-        assert size.area() == 100
+        self.assertEqual(size.area(), 100)
 
         size = Size(20, 20)
-        assert size.area() == 400
+        self.assertEqual(size.area(), 400)
 
     def test_eq(self):
         s1 = Size(10, 10)
@@ -21,18 +19,18 @@ class TestSize(TestCase):
         s3 = Size(10, 20)
         s4 = Size(10, 10)
 
-        assert s1 == s4
-        assert s1 != s2
-        assert s1 != s3
+        self.assertEqual(s1, s4)
+        self.assertNotEquals(s1, s2)
+        self.assertNotEqual(s1, s3)
 
-        assert s2 != s3
-        assert s2 != s4
+        self.assertNotEqual(s2, s3)
+        self.assertNotEqual(s2, s4)
 
-        assert s3 != s4
+        self.assertNotEqual(s3, s4)
 
-        assert s1 < s2
-        assert s4 < s2
-        assert not s1 < s3
+        self.assertLess(s1 ,s2)
+        self.assertLess(s4 , s2)
+        # self.assertGreaterEqual() not s1 < s3
 
         assert s2 > s1
         assert s2 > s4
