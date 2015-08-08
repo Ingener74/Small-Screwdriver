9b639c060538e5667b0b5a2c45250e2822e6e511
ShnaiderPavel <shnaiderpasha@gmail.com>
2015-08-08 23:14:44 +0400

    поправил тесты

--- SmallScrewdriver/Guillotine/__init__.py
+++ SmallScrewdriver/Guillotine/__init__.py
@@ -1,1 +1,1 @@
-__author__ = 'Pavel'
+from BinGuillotine import BinGuillotine
--- SmallScrewdriver/GuillotineBinPacking.py
+++ SmallScrewdriver/GuillotineBinPacking.py
@@ -1,11 +1,13 @@
 # coding=utf-8
 from SmallScrewdriver import BinPacking
+from Guillotine import BinGuillotine
 
 
 class GuillotineBinPacking(BinPacking):
-    def __init__(self, bin_size, images):
+    def __init__(self, bin_size, images, selection_heuristic=BinGuillotine):
         BinPacking.__init__(self, bin_size=bin_size)
 
+        self.selection_heuristic = selection_heuristic
         self.images = []
 
         # Bыкидываем все изображения размер которых больше или равен размеру контейнера
@@ -23,3 +25,5 @@ class GuillotineBinPacking(BinPacking):
                              key=lambda im: im.crop.size.width if im.rotated else im.crop.size.height,
                              reverse=True)
 
+        for i in self.images:
+            print i
