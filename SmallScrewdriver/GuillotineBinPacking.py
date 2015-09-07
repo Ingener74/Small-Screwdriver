8f13eba65630d289abd0921bbcc09bc3b9af6ddf
ShnaiderPavel <shnaiderpasha@gmail.com>
2015-09-07 15:55:44 +0300

    поправил работу упаковщика гильотиной

--- SmallScrewdriver/GuillotineBinPacking.py
+++ SmallScrewdriver/GuillotineBinPacking.py
@@ -12,28 +12,21 @@ class GuillotineBinPacking(BinPacking):
         select_heuristic = kwargs['select_heuristic'] if 'select_heuristic' in kwargs else BinGuillotine.AREA_FIT
         split_rule = kwargs['split_rule'] if 'split_rule' in kwargs else Rect.RULE_SAS
 
-        print len(images)
         # Отсеиваем изображения которые больше размера контейнера
         self.images = filter(lambda image: image.size.less(self.bin_size) == (True, True), images)
-        print len(self.images)
 
         # Создаём первый контейнер
         self.__newContainer(select_heuristic, select_variant, split_rule)
 
         for i in self.images:
             for b in self.bins:
-                print u'Помещаем изображение ', i
                 if b.addImage(i):
-                    self.images.remove(i)
                     break
-                else:
-                    print u'Изображение не вошло'
             else:
-                print u'Новый контейнер'
                 # Не в один из контеёнеров изображение не влезло, делаем ещё один контейнер
                 self.__newContainer(select_heuristic, select_variant, split_rule)
 
-            # break
+                # break
 
     def __newContainer(self, select_heuristic, select_variant, split_rule):
         self.bins += [BinGuillotine(self.bin_size,
