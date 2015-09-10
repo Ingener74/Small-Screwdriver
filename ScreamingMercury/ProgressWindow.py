3806d745a70838e18ae9fe9493e939fb69acdc8f
ShnaiderPavel <shnaiderpasha@gmail.com>
2015-09-10 11:59:24 +0400

    поправил окно прогресса

--- ScreamingMercury/ProgressWindow.py
+++ ScreamingMercury/ProgressWindow.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'ScreamingMercury/ProgressWindow.ui'
+# Form implementation generated from reading ui file 'C:/Users/Pavel/workspace/Small-Screwdriver/ScreamingMercury/ProgressWindow.ui'
 #
-# Created: Tue Sep  8 09:24:57 2015
+# Created: Thu Sep 10 11:58:37 2015
 #      by: pyside-uic 0.2.15 running on PySide 1.2.2
 #
 # WARNING! All changes made in this file will be lost!
@@ -12,7 +12,10 @@ from PySide import QtCore, QtGui
 class Ui_ProgressWindow(object):
     def setupUi(self, ProgressWindow):
         ProgressWindow.setObjectName("ProgressWindow")
-        ProgressWindow.resize(471, 79)
+        ProgressWindow.resize(471, 82)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(":/main/screwdriver.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        ProgressWindow.setWindowIcon(icon)
         self.verticalLayout_2 = QtGui.QVBoxLayout(ProgressWindow)
         self.verticalLayout_2.setObjectName("verticalLayout_2")
         self.groupBox = QtGui.QGroupBox(ProgressWindow)
@@ -29,6 +32,7 @@ class Ui_ProgressWindow(object):
         QtCore.QMetaObject.connectSlotsByName(ProgressWindow)
 
     def retranslateUi(self, ProgressWindow):
-        ProgressWindow.setWindowTitle(QtGui.QApplication.translate("ProgressWindow", "Form", None, QtGui.QApplication.UnicodeUTF8))
+        ProgressWindow.setWindowTitle(QtGui.QApplication.translate("ProgressWindow", "Прогресс упаковки", None, QtGui.QApplication.UnicodeUTF8))
         self.groupBox.setTitle(QtGui.QApplication.translate("ProgressWindow", "Упаковка", None, QtGui.QApplication.UnicodeUTF8))
 
+import resources_rc
